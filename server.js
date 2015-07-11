d6fab039ecda2ce30a094623726cfbcc4b44f124
Jesse Goodman <jpmg@princeton.edu>
2015-07-10 22:52:25 -0700

    port specification

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
-
+var optPort = process.argv[2];
+var listenPort;
 
 app.use(express.static(__dirname + '/public'));
 
+if (optPort)
+	listenPort = optPort;
+else
+	listenPort = 80;
 
-app.listen(3000);
-console.log('Server running on port 3000');
+app.listen(listenPort);
+console.log('Server running on port ' + listenPort);
