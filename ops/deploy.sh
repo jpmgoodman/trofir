ad64d1b40c18b7e3e912a484c171206e8819a2de
Jesse Goodman <jpmg@princeton.edu>
2015-08-23 02:24:45 -0700

    fully functional and descriptive deploy script

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,2 +1,23 @@
 #!/usr/bin/env sh
-ssh root@trofir.com 'cd /opt/mean/trofir/; eval "$(ssh-agent)"; ssh-add ~/.ssh/id_rsa; git pull origin master; exit'
+ssh root@trofir.com     'cd /opt/mean/trofir/                           &&
+                        echo --------------------------------------     ;
+                        echo adding ssh key...                          ;
+                        echo --------------------------------------     ;
+                        eval "$(ssh-agent)"                             &&
+                        ssh-add ~/.ssh/id_rsa                           &&
+                        echo --------------------------------------     ;
+                        echo successfully added ssh key!                ;
+                        echo pulling repo from github...                ;
+                        echo --------------------------------------     ;
+                        git pull origin master                          &&
+                        npm install                                     &&
+                        echo --------------------------------------     ;
+                        echo successful pull from github!               ;
+                        echo restarting server...                       ;
+                        echo --------------------------------------     ;
+                        forever stop 0                                  &&
+                        PORT=80 forever start server.js                 &&
+                        echo --------------------------------------     ;
+                        echo server successfully restarted. done!       ;
+                        echo --------------------------------------     ;
+                        exit'
